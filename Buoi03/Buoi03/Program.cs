db4eec50fa0c73821c2e4ab45ed49f5072e2f9f0
lamdat284 <dattan2804@gmail.com>
2024-07-24 09:44:34 +0700

    Upload Sum.cs

--- Buoi03/Buoi03/Program.cs
+++ Buoi03/Buoi03/Program.cs
@@ -10,6 +10,25 @@ namespace Buoi03
     {
         static void Main(string[] args)
         {
+            SumArray _sumArray = new SumArray();
+
+            Console.WriteLine("\tTONG MANG TU 1 -> n");
+            Console.Write("**Nhap n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("**PHUONG PHAP KHONG DE QUI!!!");
+           
+            int sumArray_nonRecursion = _sumArray.SumDivideAndConquer_nonRecursion(1, n);
+            Console.WriteLine("=>Ket qua khi n = " + n + " la: " + sumArray_nonRecursion);
+
+            Console.WriteLine("**PHUONG PHAP DE QUI!!!");
+            int sumArray_Recursion = _sumArray.SumDivideAndConquer_Recursion(1, n);
+            Console.WriteLine("=>Ket qua khi n = " + n + " la: " + sumArray_Recursion);
+
+
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ Buoi03/Buoi03/SumArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi03
+{
+    public class SumArray
+    {
+        public int SumDivideAndConquer_nonRecursion(int left, int right)
+        {
+            int totalSum = 0;
+
+            // Tính tổng của mảng từ left đến right bằng hai vòng lặp
+            int mid = (left + right) / 2;
+
+            // Tính tổng của phần đầu tiên của mảng từ left đến mid
+            for (int i = left; i <= mid; i++)
+            {
+                totalSum += i;
+            }
+
+            // Tính tổng của phần thứ hai của mảng từ mid + 1 đến right
+            for (int i = mid + 1; i <= right; i++)
+            {
+                totalSum += i;
+            }
+
+            return totalSum;
+        }
+        public int SumDivideAndConquer_Recursion(int left, int right)
+        {
+            if (left == right)
+            {
+                // Trường hợp cơ sở: Nếu chỉ còn một phần tử, trả về giá trị của nó
+                return left;
+            }
+
+            // Chia mảng thành hai phần
+            int mid = (left + right) / 2;
+
+            // Tính tổng của hai phần bằng cách gọi đệ quy
+            int leftSum = SumDivideAndConquer_Recursion(left, mid);
+            int rightSum = SumDivideAndConquer_Recursion(mid + 1, right);
+
+            // Cộng tổng của hai phần
+            return leftSum + rightSum;
+        }
+
+    }
+}
